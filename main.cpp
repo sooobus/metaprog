77ef016ef40d3b8fb88ce8aacf080abcb195a87d
Valeriia Nemychnikova <sooobus@gmail.com>
2018-10-18 22:51:14 +0300

    Added template for proxy / mediator distinction

--- main.cpp
+++ main.cpp
@@ -23,72 +23,73 @@
 
  */
 
-class C; // Forward declaration
+//class C; // Forward declaration
 class B;
+
+template<size_t N>
 class A;
 
 class B {
 public:
-    explicit B(size_t id_); // B must not know anything about other classes, but this id is included to demonstrate its work.
+    explicit B(
+            size_t id_); // B must not know anything about other classes, but this id is included to demonstrate its work.
 
 
     void Foo(); // Example function that is needed to be run from A (for proxy & mediator).
     size_t GetState(); // Example state is just a bool that shows that Foo was called at least once.
-    
+
 private:
     bool was_called;
-    size_t id; 
+    size_t id;
 };
 
+template<size_t N>
 class C {
 public:
-    explicit C(size_t b_number); // Number of workers (objects of class B) is known before.
+    explicit C() : call_index(0) { // Number of workers (objects of class B) is known before.
+        b_ptrs.reserve(N);
+        for (size_t i = 0; i < N; ++i)
+            b_ptrs.emplace_back(new B(i));
+    }
 
     void BFoo(); // Used as interface for calling A.Foo.
-    void AddListener(std::shared_ptr<A> ptr); // Adds new A that receives notifications.
+
+    void AddListener(std::shared_ptr<A<N>> ptr); // Adds new A that receives notifications.
     void AskBState(size_t index); // Asks specific B about its state.
 
 private:
     std::vector<std::unique_ptr<B>> b_ptrs;
-    std::vector<std::shared_ptr<A>> listeners;
+    std::vector<std::shared_ptr<A<N>>> listeners;
     size_t call_index;
 };
 
+template<size_t N>
 class A {
 public:
-    A(C* multitool_); // Uses C as proxy and / or mediator.
+    explicit A(C<N> *multitool_) : multitool(multitool_) {} // Uses C as proxy and / or mediator.
 
-    void CallFoo(); // Demonstrates calling B.Foo().
+    void CallFoo() { // Demonstrates calling B.Foo().
+        multitool->BFoo();
+    }
+
     void GetNotification(bool x); // Receives information about states that it is subscribed to.
 
 private:
-    C* multitool;
+    C<N> *multitool;
 };
 
-void A::GetNotification(bool x){
-    if(x){
+template<size_t N>
+void A<N>::GetNotification(bool x) {
+    if (x) {
         std::cout << "Received state notification from B: it was already called" << std::endl;
-    }
-    else{
+    } else {
         std::cout << "Received state notification from B: it has not been called yet" << std::endl;
     }
 }
 
-A::A(C* multitool_) : multitool(multitool_) {}
-
 B::B(size_t id_) : id(id_), was_called(false) {}
 
-C::C(size_t b_number) : call_index(0) {
-    b_ptrs.reserve(b_number);
-    for (size_t i = 0; i < b_number; ++i)
-        b_ptrs.emplace_back(new B(i));
-}
-
-void A::CallFoo() {
-    multitool->BFoo();
-}
-
-size_t B::GetState(){
+size_t B::GetState() {
     return was_called;
 }
 
@@ -97,46 +98,49 @@ void B::Foo() {
     was_called = true;
 }
 
-void C::AskBState(size_t index){
-    for(auto listener : listeners){
+template<size_t N>
+void C<N>::AskBState(size_t index) {
+    for (const auto& listener : listeners) {
         listener->GetNotification(b_ptrs[index]->GetState());
     }
 }
 
-void C::BFoo() {
+template<size_t N>
+void C<N>::BFoo() {
     b_ptrs[call_index]->Foo();
     call_index++;
     call_index %= b_ptrs.size();
 }
 
-void C::AddListener(std::shared_ptr<A> ptr){
+template<size_t N>
+void C<N>::AddListener(std::shared_ptr<A<N>> ptr) {
     listeners.push_back(ptr);
 }
 
 int main() {
-    C c(5);
-    A a1(&c);
-    A a2(&c);
-    A a3(&c);
+    C<5> c;
+    A<5> a1(&c);
+    A<5> a2(&c);
+    A<5> a3(&c);
 
-    c.AddListener(std::make_shared<A>(a1));
-    c.AddListener(std::make_shared<A>(a1));
+    c.AddListener(std::make_shared<A<5>>(a1));
+    c.AddListener(std::make_shared<A<5>>(a1));
 
-    for(size_t i = 0; i < 5; ++i)
+    for (size_t i = 0; i < 5; ++i)
         c.AskBState(i);
 
     a1.CallFoo();
-    
-    for(size_t i = 0; i < 5; ++i)
+
+    for (size_t i = 0; i < 5; ++i)
         c.AskBState(i);
 
     a2.CallFoo();
-    
-    for(size_t i = 0; i < 5; ++i)
+
+    for (size_t i = 0; i < 5; ++i)
         c.AskBState(i);
 
     a3.CallFoo();
-    for(size_t i = 0; i < 5; ++i)
+    for (size_t i = 0; i < 5; ++i)
         c.AskBState(i);
 
     return 0;
